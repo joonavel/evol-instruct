92c886f1e6d65e75f5b0a98bca4c925959611a60
joonavel <joonavel@gmail.com>
2025-05-10 16:15:54 +0900

    fix: fix typos

--- seed_data/main.py
+++ seed_data/main.py
@@ -6,7 +6,7 @@ import os, argparse
 
 def get_config():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--repo_names",
+    parser.add_argument("--repo-names",
                         type=str,
                         default="HAERAE-HUB/KMMLU,beomi/KoAlpaca-v1.1a,HumanF-MarkrAI/WIKI_QA_Near_dedup",
                         help="샘플링할 데이터셋 레포(HF), 쉼표로 구분, 기본은 KMMLU, KoAlpaca-v1.1a, WIKI_QA_Near_dedup")
@@ -14,15 +14,15 @@ def get_config():
                         type=int,
                         default=42,
                         help="Shuffle시 사용되는 seed")
-    parser.add_argument("--subset_ext_cnt",
+    parser.add_argument("--subset-ext-cnt",
                         type=int,
                         default=10,
                         help="subset을 가지고 있는 데이터셋에서 subset 마다 샘플링할 데이터 수")
-    parser.add_argument("--ext_cnt",
+    parser.add_argument("--ext-cnt",
                         type=int,
                         default=100,
                         help="subset을 가지고 있지 않은 데이터셋에서 샘플링할 데이터 수")
-    parser.add_argument("--save_path",
+    parser.add_argument("--save-path",
                         type=str,
                         default="../seed_instruction.json",
                         help="seed 데이터를 저장할 경로")
